d592887a7e51d5914d53f6ce3b7b15a3cc5fe0b1
Alif <imalif.shaikh999@gmail.com>
2024-01-31 22:06:55 +0530

    use of constructor in C++

---
+++ constructor.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+class Employee
+{
+public:
+    string name;
+    int salary;
+
+    Employee(string name, int salary, string password)
+    {
+        this->name = name;
+        this->salary = salary;
+        this->password= password;
+    }
+
+    void emp()
+    {
+        cout << "The name of our 1st employee is " << this->name << " and his salary is " << this->salary << endl;
+    }
+    void gettingPass()
+    {
+        cout<<"password is : "<<this->password<<endl;
+    }
+private:
+   string password;
+};
+
+int main()
+{
+    Employee har("ALIF", 50000, "sda848ds");
+    // har.name="ALIF";
+    // har.salary=50000;
+    // cout<<"The name of our 1st employee is "<<har.name<<" and his salary is "<<har.salary<<endl;
+    har.emp();
+    cout<<"name is : "<<har.name<<endl;
+   // cout<<"password is : "<<har.password<<endl;   //error
+   har.gettingPass();
+}
