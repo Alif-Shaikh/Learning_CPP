91d452ab7e6cd109e3ac4de9623d9dd74dfaaf95
Alif <imalif.shaikh999@gmail.com>
2024-01-31 20:52:37 +0530

    pointers in C++

---
+++ pointers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  int a=34;
+  int *ptra;  
+//   float a=34;
+//   float *ptra;  
+//   ptra=&a;
+  cout<<&a<<endl;
+  cout<<ptra<<endl;
+  cout<<*ptra<<endl; //*=>dereferencing operator
+
+}
