1bd45d9b56ff897e7ccf98a7388a398bc90a2194
Alif <imalif.shaikh999@gmail.com>
2024-01-31 18:28:27 +0530

    conditional statements in C++

---
+++ conditionals.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    // cout<<"Enter your age"<<endl;
+    // int age;
+    // cin>>age;
+    
+    //  if(age>=18 && age<125){
+    //     cout<<"you are an adult";
+    // }else if(age<18){
+    //     cout<<"you are a teen";
+    // }else if(age>60){
+    //     cout<<"invalid age";
+    // }
+
+
+// cout<<"Enter your rank" <<endl<<"1"<<endl<<"2"<<endl<<"3"<<endl;
+//  int rank;
+//     cin>>rank;
+    
+//     switch (rank)
+//     {
+//     case 1:cout<<"you are first";
+//         /* code */
+//         break;
+//         case 2: cout<<"you are second";
+//         break;
+//         case 3:cout<<"you are third";
+//         break;
+//     default:cout<<"you are no more in the race";
+//         break;
+//     }
+
+// int index=0;
+// do{
+// cout<<"index is "<<index<<endl; 
+// index++;
+// }while(index<10);
+
+
+for(int i =0;i<10;i++)
+{
+cout<<i<<endl;
+}
+   return 0;
+}
